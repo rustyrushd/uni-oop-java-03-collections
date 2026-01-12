be145c0ea6e1d7413d9977d3762878dc0592b2a6
rgm <git@rushd.xyz>
2026-01-12 21:33:07 +0000

    Added a separate class to handle input.

---
+++ src/main/java/ie/uni/collections/StudentHandler.java
@@ -0,0 +1,61 @@
+package ie.uni.collections;
+
+import java.util.ArrayList;
+import java.util.LinkedHashSet;
+import java.util.List;
+import java.util.Scanner;
+
+public class StudentHandler {
+
+  private final List<Student> studentList = new ArrayList<>();
+  private final LinkedHashSet<String> studentEmails = new LinkedHashSet<>();
+
+  // Enter count number of students and input their attributes
+  public void addStudents(Scanner scan1, int count) {
+    for (int i = 0; i < count; i++) {
+      System.out.println("Student " + (i + 1));
+      System.out.println("Please enter Student name: ");
+      String name = firstCap(scan1.nextLine().toLowerCase().trim());
+
+      System.out.println("Please enter Student email: ");
+      String email = duplicateEmailCheck(scan1, scan1.nextLine().toLowerCase().trim());
+
+      System.out.println("Please enter Course: ");
+      String course = firstCap(scan1.nextLine().toLowerCase().trim());
+      // TODO check for empty entries
+
+      studentEmails.add(email);
+      studentList.add(new Student(name, email, course));
+    }
+  }
+
+  // capitalizes the first letter in a String
+  public String firstCap(String text) {
+    char firstLetter = Character.toUpperCase(text.charAt(0));
+    String rest = text.substring(1);
+    return firstLetter + rest;
+  }
+
+  // check if email already exists and keep prompting until a unique email is entered
+  private String duplicateEmailCheck(Scanner scan2, String emailAddress) {
+    while (studentEmails.contains(emailAddress)) {
+      System.out.println("That email is already in the system. Please enter another email:");
+      emailAddress = scan2.nextLine().toLowerCase().trim();
+    }
+    return emailAddress;
+  }
+
+  public void printStudentArrayList() {
+    System.out.println("\nStudent Arraylist contains:");
+    for (Student student : studentList) {
+      System.out.println(student);
+    }
+  }
+
+  public void printEmailLinkedHashSet() {
+    System.out.println("\nEmail LinkedHashSet contains:");
+    for (String email : studentEmails) {
+      System.out.print(email + " ");
+    }
+  }
+}
