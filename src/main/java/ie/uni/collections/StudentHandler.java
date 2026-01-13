22239deb848d8690e1a1ed45b7cc1296412ab5fb
rgm <git@rushd.xyz>
2026-01-13 15:16:51 +0000

    Added Apache commons-validator to check for valid email address. Edits.

--- src/main/java/ie/uni/collections/StudentHandler.java
+++ src/main/java/ie/uni/collections/StudentHandler.java
@@ -4,28 +4,31 @@ import java.util.ArrayList;
 import java.util.LinkedHashSet;
 import java.util.List;
 import java.util.Scanner;
+import org.apache.commons.validator.routines.EmailValidator;
+
 
 public class StudentHandler {
 
-  private final List<Student> studentList = new ArrayList<>();
-  private final LinkedHashSet<String> studentEmails = new LinkedHashSet<>();
+  private final List<Student> STUDENT_LIST = new ArrayList<>();
+  private final LinkedHashSet<String> STUDENT_EMAILS = new LinkedHashSet<>();
+  private static final EmailValidator VALIDATOR = EmailValidator.getInstance();
 
-  // Input for count number of students
+  // Input attributes for count number of students
   public void addStudents(Scanner scan1, int count) {
     for (int i = 0; i < count; i++) {
-      System.out.println("Student " + (i + 1));
+      System.out.println("\nStudent " + (i + 1));
       System.out.println("Please enter Student name: ");
       String name = firstCap(scan1.nextLine().toLowerCase().trim());
 
       System.out.println("Please enter Student email: ");
-      String email = duplicateEmailCheck(scan1, scan1.nextLine().toLowerCase().trim());
+      String email = emailCheck(scan1, scan1.nextLine().toLowerCase().trim());
 
       System.out.println("Please enter Course: ");
       String course = firstCap(scan1.nextLine().toLowerCase().trim());
       // TODO: check for empty entries
 
-      studentEmails.add(email);
-      studentList.add(new Student(name, email, course));
+      STUDENT_EMAILS.add(email);
+      STUDENT_LIST.add(new Student(name, email, course));
     }
   }
 
@@ -36,25 +39,30 @@ public class StudentHandler {
     return firstLetter + rest;
   }
 
-  // Check if email already exists and keep prompting until a unique email is entered
-  private String duplicateEmailCheck(Scanner scan2, String emailAddress) {
-    while (studentEmails.contains(emailAddress)) {
-      System.out.println("That email is already in the system. Please enter another email:");
-      emailAddress = scan2.nextLine().toLowerCase().trim();
+  // Check if email is both valid and unique, keep prompting until entered
+  private String emailCheck(Scanner scan2, String emailAddress) {
+    while (!VALIDATOR.isValid(emailAddress) | STUDENT_EMAILS.contains(emailAddress)) {
+      if (!VALIDATOR.isValid(emailAddress)) {
+        System.out.println("That email is invalid. Please enter a valid email address:");
+        emailAddress = scan2.nextLine().toLowerCase().trim();
+      } else if (STUDENT_EMAILS.contains(emailAddress)) {
+        System.out.println("That email is already in the system. Please enter another email:");
+        emailAddress = scan2.nextLine().toLowerCase().trim();
+      }
     }
     return emailAddress;
   }
 
   public void printStudentArrayList() {
     System.out.println("\nStudent Arraylist contains:");
-    for (Student student : studentList) {
+    for (Student student : STUDENT_LIST) {
       System.out.println(student);
     }
   }
 
   public void printEmailLinkedHashSet() {
     System.out.println("\nEmail LinkedHashSet contains:");
-    for (String email : studentEmails) {
+    for (String email : STUDENT_EMAILS) {
       System.out.print(email + " ");
     }
   }
